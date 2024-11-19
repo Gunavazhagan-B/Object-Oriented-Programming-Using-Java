f95534935bb319931ccd7b4ed23e9847b6a860c5
Gunavazhagan-B <231501055@rajalakshmi.edu.in>
2024-11-19 19:07:34 +0530

    Add files via upload

---
+++ JavaApp/src/javaapp/JavaApp.java
@@ -0,0 +1,12 @@
+
+package javaapp;
+
+public class JavaApp {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage lp=new LoginPage();
+        lp.setVisible(true);
+        lp.pack();
+        lp.setLocationRelativeTo(null);
+    } 
+}
